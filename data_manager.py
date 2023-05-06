ae6b94fa15a6a314f146f11571a0696b86d6f956
VarshaVidya <111826112+VarshaVidya@users.noreply.github.com>
2023-05-06 20:43:56 +0800

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,28 @@
+import requests
+from pprint import pprint
+Sheety_endpoint = "https://api.sheety.co/10c8b4608cf98731e59f63baecb26874/flightDeals/sheet1"
 class DataManager:
+    def __init__(self):
+        self.datum ={}
+    # sheety request to connect to the flight deals excel sheet
+    def sheety(self):
+        response = requests.get(url = Sheety_endpoint)
+        data = response.json()
+        self.datum = data["sheet1"]
+        # pprint(self.datum)
+        return self.datum
+    # print(response.text)
+
+# put request to add to the excel sheet the iata code
+    def code_update(self):
+        for city in self.datum:
+            new_datum = {
+                "sheet1":{
+                    "iataCode":city["iataCode"]
+                }
+            }
+            response = requests.put(url =f"{Sheety_endpoint}/{city['id']}",
+                                    json = new_datum)
+            print(response.text)
+
     #This class is responsible for talking to the Google Sheet.
-    pass
